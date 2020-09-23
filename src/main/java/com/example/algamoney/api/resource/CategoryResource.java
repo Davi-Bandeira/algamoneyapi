810d6e9474edb855de084c8969d352e01e4b7a5c
= <=>
2020-09-23 13:59:28 -0300

    Aula 04 - Consultando primeiro recurso com GET

---
+++ src/main/java/com/example/algamoney/api/resource/CategoryResource.java
@@ -0,0 +1,25 @@
+package com.example.algamoney.api.resource;
+
+import java.util.List;
+
+import com.example.algamoney.api.model.Category;
+import com.example.algamoney.api.repository.CategoryRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryResource {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @GetMapping
+    public List<Category> list(){
+        return categoryRepository.findAll();
+    }
+
+}
