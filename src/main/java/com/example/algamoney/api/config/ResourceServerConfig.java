45d983c51e9379077d27eb48021292c7a6d33c24
= <=>
2020-09-29 14:52:32 -0300

    Aula 6.5 -  Configurando JWT no projeto

--- src/main/java/com/example/algamoney/api/config/ResourceServerConfig.java
+++ src/main/java/com/example/algamoney/api/config/ResourceServerConfig.java
@@ -1,9 +1,7 @@
 package com.example.algamoney.api.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,12 +20,6 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("admin").password("{noop}admin").roles("ROLE");
     }
-
-//    @Override
-//    @Bean
-//    public AuthenticationManager authenticationManagerBean() throws Exception {
-//        return super.authenticationManagerBean();
-//    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
